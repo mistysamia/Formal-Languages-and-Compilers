3d3e245a6dc7b6c6b250334f80d533d33f4fb969
Misty Samia <62679682+mistysamia@users.noreply.github.com>
2022-02-25 11:49:51 +0600

    Add files via upload

---
+++ Assignment 5/Code_1.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+char input[3000];
+int index = 0;
+
+int X()
+{
+    if (input[index] == 'b' && index < strlen(input))
+    {
+        index++;
+        if ((input[index] == 'b' || input[index] == 'c') && index < strlen(input))
+        {
+            index++;
+            if (X())
+                return 1;
+
+            return 0;
+        }
+        return 0;
+    }
+    return 1;
+}
+int A()
+{
+    if (input[index] == 'a' && index < strlen(input))
+    {
+        index++;
+        int x1 = X();
+
+        if (x1)
+        {
+            if (input[index] == 'd')
+                return 1;
+        }
+        return 0;
+    }
+    return 0;
+}
+
+int main()
+{
+    printf("Enter the string : ");
+    gets(input);
+
+    if (A() && index + 1 == strlen(input))
+        printf("String Accepted\n");
+    else
+        printf("String Rejected\n");
+}
